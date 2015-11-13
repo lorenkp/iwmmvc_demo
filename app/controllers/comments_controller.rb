c5d2f8018bbce516fb2434dc8b74a4f795a8f287
Loren Peterson <lorenkp@gmail.com>
2015-11-13 11:01:32 -0800

    Can create comments for posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,33 @@
 require_relative '../../lib/controller_base'
 require_relative '../models/comment'
+require_relative '../models/post'
 
 class CommentsController < ControllerBase
+  def index
+    @comments = Post.find(post_id).comments
+    @post_id = post_id
+  end
 
+  def new
+    @post = Post.find(post_id)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to("/posts/#{comment_params[:post_id]}/comments")
+  end
+
+  private
+
+  def post_id
+    req.path.scan(/\d+/)[0]
+  end
+
+  def comment_params
+    {
+      :body => params['comment']['body'],
+      :post_id => params['comment']['post_id']
+    }
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ require_relative '../../lib/model_base'
 
 class Comment < ModelBase
   belongs_to :post
+  make_column_attr_accessors!
 end
