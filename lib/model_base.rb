1bd450b2cb47d22beaafb208f3484bbba29a7b64
Loren Peterson <lorenkp@gmail.com>
2015-12-08 12:36:41 -0800

    Back to dbname

--- lib/model_base.rb
+++ lib/model_base.rb
@@ -5,10 +5,10 @@ require_relative 'associatable'
 # Database = PG::Connection.open(:dbname => 'mvc')
 
 Database = PG::Connection.open(host: 'ec2-107-21-221-59.compute-1.amazonaws.com',
-                               database: 'de8khgpvn6f9e',
                                port: 5432,
-                               password: 'KtENQfogvyPRDC-IdkfzSgVnRC',
-                               user: 'ddgdbhyinirsoj'
+                               dbname: 'de8khgpvn6f9e',
+                               user: 'ddgdbhyinirsoj',
+                               password: 'KtENQfogvyPRDC-IdkfzSgVnRC'
                               )
 
 class ModelBase
